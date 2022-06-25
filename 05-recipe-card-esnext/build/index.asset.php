09a7693aa2d3cba2d8e004b377eca76b2b4f2e3e
descifle <descifle_itb@yahoo.com>
2022-06-25 17:15:15 -0400

    successfully ran npm run build in 05-recipe-card-esnext. Now pushing the last change after that.

--- 05-recipe-card-esnext/build/index.asset.php
+++ 05-recipe-card-esnext/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3dc9737957cfe7129ccb');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '46c15a25da777cfdd99e');
